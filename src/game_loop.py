91ef6a2f2e407a38e43390010aaeccff3d967e25
psykadel <kbackmeyer@gmail.com>
2025-04-07 22:18:36 -0500

    Removed D to debug shortcut and set constants.

--- src/game_loop.py
+++ src/game_loop.py
@@ -532,29 +532,6 @@ class Game:
                         # Spawn the powerup
                         self._spawn_powerup_of_type(i, x_pos, y_pos)
                 
-                elif event.key == pygame.K_d:
-                    # Debug mode - toggle force enemy type
-                    global DEBUG_FORCE_ENEMY_TYPE, DEBUG_ENEMY_TYPE_INDEX
-                    
-                    # If not already enabled, enable and set to enemy type 0
-                    if not DEBUG_FORCE_ENEMY_TYPE:
-                        DEBUG_FORCE_ENEMY_TYPE = True
-                        DEBUG_ENEMY_TYPE_INDEX = 0
-                        logger.info(f"DEBUG: Forcing enemy type {DEBUG_ENEMY_TYPE_INDEX} ({ENEMY_TYPE_NAMES.get(DEBUG_ENEMY_TYPE_INDEX)})")
-                    else:
-                        # If already enabled, cycle to next enemy type
-                        DEBUG_ENEMY_TYPE_INDEX = (DEBUG_ENEMY_TYPE_INDEX + 1) % 6
-                        # If we've cycled through all types, disable the feature
-                        if DEBUG_ENEMY_TYPE_INDEX == 0:
-                            DEBUG_FORCE_ENEMY_TYPE = False
-                            logger.info("DEBUG: Enemy type forcing disabled")
-                        else:
-                            logger.info(f"DEBUG: Forcing enemy type {DEBUG_ENEMY_TYPE_INDEX} ({ENEMY_TYPE_NAMES.get(DEBUG_ENEMY_TYPE_INDEX)})")
-                    
-                    # Show an on-screen notification
-                    text = f"DEBUG: Forcing {ENEMY_TYPE_NAMES.get(DEBUG_ENEMY_TYPE_INDEX)}" if DEBUG_FORCE_ENEMY_TYPE else "DEBUG: Normal enemy spawning"
-                    self.notifications.add(PowerupNotification(text, (255, 255, 0), (SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2), self.notifications))
-                
             elif event.type == pygame.KEYUP:
                 if event.key == pygame.K_SPACE:
                     self.player.stop_firing()
@@ -1663,8 +1640,8 @@ class Game:
             "B - Scatter Bomb",
             # Removed SHIFT mechanic
             "M - Toggle Music",
-            "+/- - Volume",
-            "D - Debug Enemy Types"
+            "+/- - Volume"
+            # Removed "D - Debug Enemy Types" line
         ]
         
         y_pos = SCREEN_HEIGHT - 10 - (len(controls) * 22)  # Increased spacing
